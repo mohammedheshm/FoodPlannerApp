89a7b43ef4175b4af5eb287ab13f9598ea8d8d7a
Mo7ammed He4am <mohammedhe4am@gmail.com>
2024-08-12 20:17:36 +0300

    Create Sign Up Or Login Screen Home

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -37,10 +38,19 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
     //Lottie Animation
     implementation ("com.airbnb.android:lottie:3.4.0")
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/foodplannerapp/sign_in_with_google/SignUpOrLoginActivity.java
+++ app/src/main/java/com/example/foodplannerapp/sign_in_with_google/SignUpOrLoginActivity.java
@@ -15,12 +15,8 @@ public class SignUpOrLoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up_or_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
     }
 }
