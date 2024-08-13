7399c52d8642f4e9410c94834cbd3f6a0e6e892d
Mo7ammed He4am <mohammedhe4am@gmail.com>
2024-08-14 00:20:42 +0300

    Create Meals Plan Interface Dao For Room Database

---
+++ app/src/main/java/com/example/foodplannerapp/data/room/PlaneFoodDAO.java
@@ -0,0 +1,33 @@
+package com.example.foodplannerapp.data.room;
+
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import com.example.foodplannerapp.data.model.meals.MealPlan;
+import java.util.List;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Single;
+
+@Dao
+public interface PlaneFoodDAO {
+    @Query("SELECT * FROM MealPlan")
+    public Single<List<MealPlan>> showPlanMeals();
+
+    @Query("DELETE FROM meals")
+    public Completable removeAllTable();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public Completable insertPlanMeal(MealPlan mealPlan);
+
+    @Delete
+    public Completable deletePlanMeal(MealPlan mealPlan);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public Completable insertAllTable(List<MealPlan> mealPlanList);
+
+    @Query("SELECT * FROM MealPlan where day=:dayName" )
+    public Single<List<MealPlan>> showPlanMealsByDay(Week dayName);
+}
