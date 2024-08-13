38a5d0475290d98ed5e5e9bcba5cdbc1811bafcc
Mo7ammed He4am <mohammedhe4am@gmail.com>
2024-08-14 00:06:01 +0300

    Create Retrofit Network with rx java

---
+++ app/src/main/java/com/example/foodplannerapp/data/network/Network.java
@@ -0,0 +1,40 @@
+package com.example.foodplannerapp.data.network;
+
+import android.content.Context;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Network {
+    private volatile static Retrofit instance = null;
+
+    private Network() {
+    }
+
+    public static synchronized Retrofit getInstance() {
+        if (instance == null) {
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+            Gson gson = new GsonBuilder().setLenient().create();
+            instance = new Retrofit.Builder()
+                    .baseUrl(ApiCalls.BASE_URL)
+                    .client(client)
+                    .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+        return instance;
+
+    }
+
+    public static ApiCalls apiCalls = getInstance().create(ApiCalls.class);
+
+}
