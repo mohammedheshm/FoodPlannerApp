30a9743119f03698e382a40b45fce63098f561da
Mo7ammed He4am <mohammedhe4am@gmail.com>
2024-08-13 16:45:24 +0300

    Create Category Models

---
+++ app/src/main/java/com/example/foodplannerapp/data/model/category/CategoriesList.java
@@ -0,0 +1,28 @@
+package com.example.foodplannerapp.data.model.category;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriesList {
+
+    @SerializedName("meals")
+    private List<Category> categories = new ArrayList<>();
+
+    public void setCategory(List<Category> categories){
+        this.categories = categories;
+    }
+
+    public List<Category> getCategory(){
+        return categories;
+    }
+
+    @Override
+    public String toString(){
+        return
+                "Categories{" +
+                        "Category = '" + categories + '\'' +
+                        "}";
+    }
+}
---
+++ app/src/main/java/com/example/foodplannerapp/data/room/Week.java
@@ -0,0 +1,12 @@
+package com.example.foodplannerapp.data.room;
+
+
+public enum Week {
+    SATURDAY,
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY
+}
